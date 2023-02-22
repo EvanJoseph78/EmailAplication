c0286c40ccd3201f416f8221cbc3f00d5c8be4db
Evan Joseph <evandromariano49@gmail.com>
2023-02-22 11:23:47 -0300

    first commit

---
+++ src/main/java/com/mycompany/emailaplication/EmailAplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.emailaplication;
+
+/**
+ *
+ * @author evan
+ */
+public class EmailAplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
